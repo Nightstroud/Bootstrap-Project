55675516c0b1eaa150989665fb66e488876d362a
Nightstroud <54329342+Nightstroud@users.noreply.github.com>
2019-10-02 10:02:00 -0600

    Add files via upload

---
+++ Bootstrap/random.js
@@ -0,0 +1,5 @@
+let myArray = ["This is number 1", "This is number 2", "This is number 3", "This is number 4", "This is number 5"];
+
+for (let i = myArray.length; i > -1; i--) {
+    console.log(myArray[i]);
+}
